1ab519fe5d861aada552f0f021bd27eab59c6280
Vito Minheere <vitominheere@gmail.com>
2022-10-24 21:13:26 +0200

    Get started on models

--- django/nextbestrest/models.py
+++ django/nextbestrest/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    creation_date = models.DateTimeField()
+    score = models.IntegerField()
+    text = models.CharField(max_length=1000)
